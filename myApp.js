e165591f5129dde9d18caff5c4ea6cddbe8884e3
Bill <billcheptoyek60@gmail.com>
2025-05-30 01:47:56 +0300

    createamnypeople

--- myApp.js
+++ myApp.js
@@ -29,7 +29,10 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, people) => {
+    if (err) return done(err);
+    done(null, people);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
